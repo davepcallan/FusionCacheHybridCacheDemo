b768e9ed6a71b02ecf3c5b9eb3a79d52c3e04081
David Callan <davepcallan@gmail.com>
2025-01-28 02:58:23 +0000

    Add Fusion Cache as HybridCache and add hybrid cache endpoint for getting currencies.

--- FusionCacheHybridCacheDemo/Program.cs
+++ FusionCacheHybridCacheDemo/Program.cs
@@ -1,10 +1,48 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using ZiggyCreatures.Caching.Fusion;
+using ZiggyCreatures.Caching.Fusion.Serialization.SystemTextJson;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddFusionCache()
+    .WithSerializer(new FusionCacheSystemTextJsonSerializer())
+    .WithDistributedCache(
+        new RedisCache(new RedisCacheOptions
+        {
+            Configuration = builder.Configuration["RedisCache:Configuration"],
+            InstanceName = builder.Configuration["RedisCache:InstanceName"]
+        })
+    ).AsHybridCache();
+
 var app = builder.Build();
+
+app.MapGet("/currencies/hybrid-cache", async (AppDbContext dbContext, HybridCache hybridCache, CancellationToken cancellationToken) =>
+{
+    const string cacheKey = "currency_list_hybrid";
+    var currencies = await hybridCache.GetOrCreateAsync(
+        cacheKey,
+
+        async _ => await dbContext.Currencies
+            .AsNoTracking()
+            .TagWith("HYBRID-CACHE")
+            .ToListAsync(cancellationToken),
+
+        new HybridCacheEntryOptions
+        {
+            LocalCacheExpiration = TimeSpan.FromSeconds(20),
+            Expiration = TimeSpan.FromMinutes(1)
+        },
+
+        cancellationToken: cancellationToken
+    );
+
+    return currencies;
+});
 
 app.MapGet("/currencies/no-cache", async (AppDbContext dbContext) =>
 {
